06b2911fb6bffd2c610ca595327f37abec42cb3c
widjaja <wwidjaja0@gmail.com>
2024-09-02 17:10:05 -0700

    Update Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import myLogo from '/logo.svg'
 import './App.css'
 
 function App() {
@@ -9,14 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://wwidjaja.site" target="_blank">
+          <img src={myLogo} className="logo" alt="Widjaja's Logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>William Widjaja</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the logos to learn more
       </p>
     </>
   )
